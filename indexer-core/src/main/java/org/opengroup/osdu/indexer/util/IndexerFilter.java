c750426cf1ce54dcef5ad9a4b3ebf7eaa5ee84fc
Mina Otgonbold <AOtgonbold@slb.com>
2022-04-19 09:18:39 -0500

    Add cache-control headers for an SLB specific API

--- indexer-core/src/main/java/org/opengroup/osdu/indexer/util/IndexerFilter.java
+++ indexer-core/src/main/java/org/opengroup/osdu/indexer/util/IndexerFilter.java
@@ -2,6 +2,7 @@
 package org.opengroup.osdu.indexer.util;
 
 import java.io.IOException;
+import java.util.Map;
 import javax.servlet.Filter;
 import javax.servlet.FilterChain;
 import javax.servlet.FilterConfig;
@@ -10,8 +11,10 @@ import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletResponse;
 import lombok.extern.java.Log;
+import org.opengroup.osdu.core.common.http.ResponseHeadersFactory;
 import org.opengroup.osdu.core.common.model.http.DpsHeaders;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Log
@@ -19,6 +22,11 @@ import org.springframework.stereotype.Component;
 public class IndexerFilter implements Filter {
 
     private final DpsHeaders dpsHeaders;
+    private ResponseHeadersFactory responseHeadersFactory = new ResponseHeadersFactory();
+
+    // defaults to * for any front-end, string must be comma-delimited if more than one domain
+    @Value("${ACCESS_CONTROL_ALLOW_ORIGIN_DOMAINS:*}")
+    String ACCESS_CONTROL_ALLOW_ORIGIN_DOMAINS;
 
     @Autowired
     public IndexerFilter(DpsHeaders dpsHeaders) {
@@ -37,9 +45,18 @@ public class IndexerFilter implements Filter {
         HttpServletResponse httpResponse = (HttpServletResponse) servletResponse;
 
         dpsHeaders.addCorrelationIdIfMissing();
-        httpResponse.addHeader(DpsHeaders.CORRELATION_ID, dpsHeaders.getCorrelationId());
+
+        setResponseHeaders(httpResponse);
 
         filterChain.doFilter(servletRequest, servletResponse);
+    }
+
+    private void setResponseHeaders(HttpServletResponse httpServletResponse) {
+        Map<String, String> responseHeaders = responseHeadersFactory.getResponseHeaders(ACCESS_CONTROL_ALLOW_ORIGIN_DOMAINS);
+        for (Map.Entry<String, String> header : responseHeaders.entrySet()) {
+            httpServletResponse.addHeader(header.getKey(), header.getValue());
+        }
+        httpServletResponse.addHeader(DpsHeaders.CORRELATION_ID, this.dpsHeaders.getCorrelationId());
     }
 
     @Override
