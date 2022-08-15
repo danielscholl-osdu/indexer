db96ef441c2bdf2a9b059ba14c0a171405b17ad3
ZMai <zmai@slb.com>
2022-08-15 08:13:13 -0500

    Add Schlumberger attributions in new files and remove the unused method in class VirtualProperties

--- indexer-core/src/main/java/org/opengroup/osdu/indexer/schema/converter/tags/VirtualProperties.java
+++ indexer-core/src/main/java/org/opengroup/osdu/indexer/schema/converter/tags/VirtualProperties.java
@@ -1,3 +1,17 @@
+// Copyright 2017-2020, Schlumberger
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//      http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 package org.opengroup.osdu.indexer.schema.converter.tags;
 
 import com.fasterxml.jackson.annotation.JsonAnyGetter;
@@ -10,10 +24,6 @@ import java.util.Map;
 @Data
 public class VirtualProperties {
     private Map<String, VirtualProperty> items = new HashMap<>();
-
-    public VirtualProperty getProperty(String name) {
-        return items.get(name);
-    }
 
     @JsonAnySetter
     public void add(String key, VirtualProperty value) {
