3042d5e1f63e31153cfdb754cab3f58089a0c991
ZMai <zmai@slb.com>
2022-08-18 09:28:14 -0500

    Refactor the names of the arguments and update the comment to make the method more readable

--- indexer-core/src/main/java/org/opengroup/osdu/indexer/util/VirtualPropertyUtil.java
+++ indexer-core/src/main/java/org/opengroup/osdu/indexer/util/VirtualPropertyUtil.java
@@ -21,11 +21,11 @@ public class VirtualPropertyUtil {
     private static final String PROPERTY_DELIMITER = ".";
     private static final String DATA_PREFIX = "data" + PROPERTY_DELIMITER;
 
-    public static boolean isPropertyPathMatched(String path, String propertyPath) {
-        // We should not just use name.startsWith(originalPropertyPath)
-        // For example, if originalPropertyPath is "FacilityName" and name.startsWith(originalPropertyPath) is used,
-        // it can match property "FacilityNameAlias". Same in method chooseOriginalProperty(...)
-        return !Strings.isNullOrEmpty(path) && (path.startsWith(propertyPath + PROPERTY_DELIMITER) || path.equals(propertyPath));
+    public static boolean isPropertyPathMatched(String propertyPath, String propertyPathPrefix) {
+        // We should not just use propertyPath.startsWith(propertyPathPrefix)
+        // For example, if propertyPathPrefix is "data.FacilityName" and propertyPath.startsWith(propertyPathPrefix) is used,
+        // then the property "data.FacilityNameAlias" will be matched and unexpected result will be returned.
+        return !Strings.isNullOrEmpty(propertyPath) && (propertyPath.startsWith(propertyPathPrefix + PROPERTY_DELIMITER) || propertyPath.equals(propertyPathPrefix));
     }
 
     public static String removeDataPrefix(String path) {
