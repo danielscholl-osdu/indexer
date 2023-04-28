850a6209042b1a26850fd5f4f6b0dcafa985ad63
ZMai <zmai@slb.com>
2023-04-28 03:40:06 -0500

    Update unit tests

--- indexer-core/src/test/java/org/opengroup/osdu/indexer/service/IndexerSchemaServiceTest.java
+++ indexer-core/src/test/java/org/opengroup/osdu/indexer/service/IndexerSchemaServiceTest.java
@@ -14,6 +14,7 @@
 
 package org.opengroup.osdu.indexer.service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 import org.apache.http.HttpStatus;
@@ -301,6 +302,69 @@ public class IndexerSchemaServiceTest {
         verify(this.schemaCache, times(1)).delete(any());
         verify(this.flattenedSchemaCache, times(1)).delete(any());
         verify(this.virtualPropertiesSchemaCache, times(1)).delete(any());
+    }
+
+    @Test
+    public void should_get_schema_of_related_object_kinds_when__kind_has_property_configuration() throws IOException, URISyntaxException {
+        String kind = "tenant1:avocet:completion:1.0.0";
+        String storageSchema = "{" +
+                "  \"kind\": \"tenant1:avocet:completion:1.0.0\"," +
+                "  \"schema\": [" +
+                "    {" +
+                "      \"path\": \"status\"," +
+                "      \"kind\": \"string\"" +
+                "    }," +
+                "    {" +
+                "      \"path\": \"startDate\"," +
+                "      \"kind\": \"string\"" +
+                "    }" +
+                "  ]" +
+                "}";
+        String propertyConfigurations = "{\n" +
+                "    \"Name\": \"WellLogIndex-PropertyPathConfiguration\",\n" +
+                "    \"Code\": \"osdu:wks:work-product-component--WellLog:1.\",\n" +
+                "    \"AttributionAuthority\": \"OSDU\",\n" +
+                "    \"Configurations\": [{\n" +
+                "            \"Name\": \"WellboreName\",\n" +
+                "            \"Policy\": \"ExtractFirstMatch\",\n" +
+                "            \"Paths\": [{\n" +
+                "                    \"RelatedObjectsSpec.RelationshipDirection\": \"ChildToParent\",\n" +
+                "                    \"RelatedObjectsSpec.RelatedObjectKind\": \"osdu:wks:master-data--Wellbore:1.\",\n" +
+                "                    \"RelatedObjectsSpec.RelatedObjectID\": \"data.WellboreID\",\n" +
+                "                    \"ValueExtraction.ValuePath\": \"data.FacilityName\"\n" +
+                "                }\n" +
+                "            ]\n" +
+                "        }, {\n" +
+                "            \"Name\": \"TechnicalAssuranceReviewerOrganisationNames\",\n" +
+                "            \"Policy\": \"ExtractAllMatches\",\n" +
+                "            \"Paths\": [{\n" +
+                "                    \"RelatedObjectsSpec.RelationshipDirection\": \"ChildToParent\",\n" +
+                "                    \"RelatedObjectsSpec.RelatedObjectKind\": \"osdu:wks:master-data--Organisation:1.\",\n" +
+                "                    \"RelatedObjectsSpec.RelatedObjectID\": \"data.TechnicalAssurances[].Reviewers[].OrganisationID\",\n" +
+                "                    \"ValueExtraction.ValuePath\": \"data.OrganisationName\"\n" +
+                "                }\n" +
+                "            ]\n" +
+                "        }\n" +
+                "    ]\n" +
+                "}";
+        ObjectMapper objectMapper = new ObjectMapper();
+        PropertyConfigurations configurations = objectMapper.readValue(propertyConfigurations, PropertyConfigurations.class);
+
+        when(this.elasticIndexNameResolver.getIndexNameFromKind(kind)).thenReturn(kind.replace(":", "-"));
+        when(this.schemaCache.get(kind)).thenReturn(null);
+        when(this.indicesService.isIndexExist(any(), any())).thenReturn(true);
+        when(this.schemaService.getSchema(kind)).thenReturn(storageSchema);
+        when(this.propertyConfigurationsService.getPropertyConfigurations(kind)).thenReturn(configurations);
+        when(this.propertyConfigurationsService.resolveConcreteKind(anyString())).thenAnswer(invocation -> {
+            String relatedObjectKind = invocation.getArgument(0);
+            return relatedObjectKind + "0.0";
+        });
+
+        IndexSchema indexSchema = this.sut.getIndexerInputSchema(kind, false);
+        assertEquals(2, indexSchema.getDataSchema().size());
+        verify(this.propertyConfigurationsService, times(2)).resolveConcreteKind(any());
+        verify(this.schemaCache, times(3)).get(any());
+        verify(this.schemaService, times(3)).getSchema(any());
     }
 
     @Test
