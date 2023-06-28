8cffa9756c710ea9b82c46279e784c90d871b454
ZMai <zmai@slb.com>
2023-06-28 17:01:57 -0500

    Remove unnecessary annotation

--- provider/indexer-azure/src/main/java/org/opengroup/osdu/indexer/azure/cache/RecordChangeInfoRedisCache.java
+++ provider/indexer-azure/src/main/java/org/opengroup/osdu/indexer/azure/cache/RecordChangeInfoRedisCache.java
@@ -18,7 +18,6 @@ package org.opengroup.osdu.indexer.azure.cache;
 import org.opengroup.osdu.core.common.cache.RedisCache;
 import org.opengroup.osdu.indexer.cache.IRecordChangeInfoCache;
 import org.opengroup.osdu.indexer.model.RecordChangeInfo;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
@@ -32,7 +31,7 @@ public class RecordChangeInfoRedisCache extends RedisCache<String, RecordChangeI
                                    final @Named("REDIS_PORT") int port,
                                    final @Named("REDIS_PASSWORD") String password,
                                    final @Named("RECORDS_REDIS_TTL") int timeout,
-                                   @Qualifier("") @Value("${redis.database}") final int database) {
+                                   @Value("${redis.database}")  final int database) {
         super(host, port, password, timeout, database, String.class, RecordChangeInfo.class);
     }
 }
--- provider/indexer-azure/src/main/java/org/opengroup/osdu/indexer/azure/cache/RelatedObjectRedisCache.java
+++ provider/indexer-azure/src/main/java/org/opengroup/osdu/indexer/azure/cache/RelatedObjectRedisCache.java
@@ -18,7 +18,6 @@ package org.opengroup.osdu.indexer.azure.cache;
 import org.opengroup.osdu.core.common.cache.RedisCache;
 import org.opengroup.osdu.core.common.model.storage.RecordData;
 import org.opengroup.osdu.indexer.cache.IRelatedObjectCache;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
 import org.springframework.stereotype.Component;
@@ -32,7 +31,7 @@ public class RelatedObjectRedisCache  extends RedisCache<String, RecordData> imp
                                    final @Named("REDIS_PORT") int port,
                                    final @Named("REDIS_PASSWORD") String password,
                                    final @Named("RECORDS_REDIS_TTL") int timeout,
-                                   @Qualifier("") @Value("${redis.database}") final int database) {
+                                   @Value("${redis.database}")  final int database) {
         super(host, port, password, timeout, database, String.class, RecordData.class);
     }
 }
